b3769d831560375bff0309ae0c4ac0108ec10062
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-19 14:52:43 +0530

    Exercise 3.8*: Phonebook backend Step-8

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-
 const morgan = require('morgan')
-app.use(morgan('tiny'))
 
 app.use(express.json())
+
+morgan.token('data', (request) => {
+    if (request.method === 'POST') {
+        return JSON.stringify(request.body)
+    }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
 
 let persons = [
     { 
