d308a0c665b04b7fbc9da476fd6aed3131431a7e
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-19 22:05:53 +0530

    Exercise 3.13: Phonebook database, Step-1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -57,7 +59,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -102,7 +106,7 @@ app.post('/api/persons', (request, response) => {
     response.status(201).json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
