cc2420af309877113c06f5e3352ce18e8373dce4
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-20 14:19:24 +0530

    uibuild

--- index.js
+++ index.js
@@ -97,9 +97,9 @@ app.post('/api/persons', (request, response, next) => {
                 person.save().then(savedNote => {
                     response.status(201).json(savedNote)
                 })
+                .catch(error => next(error))
               }
         })
-        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -132,9 +132,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    } 
+    } else if (error.name === 'ValidationError') {    
+        return response.status(400).json({ error: error.message })  
+    }
   
     next(error)
   }
