0ddd3068d6d468c2b8825c163da509b3015066ad
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2022-06-20 14:54:50 +0530

    Implementing customValidation

--- models/person.js
+++ models/person.js
@@ -25,8 +25,11 @@ const personSchema = new mongoose.Schema({
         type: String,
         minLength: 8,
         validate: {
-            validator: function(v) {
-              return /\d{2,3}-\d{1,}/.test(v) || /\d{1,}/.test(v);
+            validator: function(value) {
+                if (value.includes("-")) {
+                    return /\d{2,3}-\d{1,}/.test(v)
+                }
+                return /\d{1,}/.test(v)
             },
             message: props => `${props.value} is not a valid phone number!`
           },
